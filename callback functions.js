1d382d2051fc3fc069b7392aba3d573eb2407095
Irek-91 <shamilov.irek@bk.ru>
2023-03-20 19:22:37 +0300

    Функции-«колбэки»

---
+++ callback functions.js
@@ -0,0 +1,19 @@
+//
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+    } function showOk() {
+    alert( "Вы согласны." );
+    } function showCancel() {
+    alert( "Вы отменили выполнение." );
+    } // использование: функции showOk, showCancel передаются в качестве аргументов ask
+    ask("Вы согласны?", showOk, showCancel);
+
+//Мы можем переписать этот пример значительно короче, используя Function Expression:
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+    } ask("Вы согласны?",
+        function() { alert("Вы согласились."); },
+        function() { alert("Вы отменили выполнение."); }
+    );
